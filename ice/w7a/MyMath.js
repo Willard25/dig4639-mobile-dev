60d045f02ba3467dbcde5c4b6f08d8d99a094007
Willard25 <47044174+Willard25@users.noreply.github.com>
2020-02-17 12:24:30 -0500

    w7a

--- ice/w7a/MyMath.js
+++ ice/w7a/MyMath.js
@@ -1,3 +1,55 @@
 export function Sum (a, b) {
-  return a + b
+  let result = undefined
+  if(typeof a == "number" && typeof b == "number") {
+    result = a + b
+  }
+  return result;
 }
+
+export function AddList (arr) {
+  let result = undefined;
+  if(Array.isArray(arr) && arr.length > 0) {
+    result = 0
+    for(var elem of arr){
+      if(typeof arr != "number") {
+        result = undefined;
+        break;
+      }
+      result = result + arr[i];
+    }
+  }
+  return result;
+}
+
+export function DivideBy () {
+  let result = undefined
+  if (typeof a == "number" && typeof b == "number" && b != 0) {
+    result = a / b
+  }
+  return result;
+}
+
+export function ContainsString () {
+  let result = undefined
+  if (z) {
+    result = example
+  }
+  return result;
+}
+
+export function ReSortedNumbers () {
+  let result = undefined
+  if (z) {
+    result = example
+  }
+  return result;
+}
+
+export function Adder () {
+  let result = undefined
+  if (z) {
+    result = example
+  }
+  return result;
+}
+export { Sum, AddList };
